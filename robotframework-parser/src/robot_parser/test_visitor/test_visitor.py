eb3a5f360a14be31da45ebe5af558f4c0c5c98e4
ltsuda <leonardo.tsuda@icloud.com>
2020-10-26 19:03:34 -0300

    add a tests object to hold the list of test cases

--- robotframework-parser/src/robot_parser/test_visitor/test_visitor.py
+++ robotframework-parser/src/robot_parser/test_visitor/test_visitor.py
@@ -1,12 +1,13 @@
 from robot.api import ExecutionResult, ResultVisitor
 
-test_object = []
+tests_object = {}
+test_list = []
 
 
 class TestStatus(ResultVisitor):
 
     def start_test(self, test):
-        global test_object
+        global test_list
         object = {
             'parent': test.parent,
             'name': test.name,
@@ -15,10 +16,11 @@ class TestStatus(ResultVisitor):
             'endtime': test.endtime,
             'elapsed(s)': test.elapsedtime/float(1000)
         }
-        test_object.append(object)
+        test_list.append(object)
 
     @staticmethod
     def get_test_status(inpath):
         result = ExecutionResult(inpath)
         result.visit(TestStatus())
-        return test_object
+        tests_object['tests'] = test_list
+        return tests_object
