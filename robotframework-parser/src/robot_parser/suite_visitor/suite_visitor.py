0902bbb1a973229c03d360841dd3ed7c3f9ad5e1
ltsuda <leonardo.tsuda@icloud.com>
2021-01-27 23:50:35 -0300

    convert suite/test start and endtime to Sao Paulo timezone and timestamp format

--- robotframework-parser/src/robot_parser/suite_visitor/suite_visitor.py
+++ robotframework-parser/src/robot_parser/suite_visitor/suite_visitor.py
@@ -1,3 +1,4 @@
+from ..utils.timezone_converter import *
 from robot.api import ExecutionResult, ResultVisitor
 
 suites_object = {}
@@ -5,19 +6,19 @@ suite_list = []
 
 
 class SuiteStatus(ResultVisitor):
-
     def start_suite(self, suite):
         global suite_list
         stats = suite.statistics
         object = {
-            'name': suite,
-            'status': suite.status,
-            'total': stats.all.total,
-            'passed': stats.all.passed,
-            'failed': stats.all.failed,
-            'starttime': suite.starttime,
-            'endtime': suite.endtime,
-            'elapsed(s)': suite.elapsedtime/float(1000)
+            "name": suite,
+            "longname": suite.longname,
+            "status": suite.status,
+            "total": stats.all.total,
+            "passed": stats.all.passed,
+            "failed": stats.all.failed,
+            "starttime": datetime_to_utc(suite.starttime),
+            "endtime": datetime_to_utc(suite.endtime),
+            "elapsed(s)": suite.elapsedtime / float(1000),
         }
         suite_list.append(object)
 
@@ -25,5 +26,5 @@ class SuiteStatus(ResultVisitor):
     def get_suite_status(inpath):
         result = ExecutionResult(inpath)
         result.visit(SuiteStatus())
-        suites_object['suites'] = suite_list
+        suites_object["suites"] = suite_list
         return suites_object
--- robotframework-parser/src/robot_parser/test_visitor/test_visitor.py
+++ robotframework-parser/src/robot_parser/test_visitor/test_visitor.py
@@ -1,3 +1,4 @@
+from ..utils.timezone_converter import *
 from robot.api import ExecutionResult, ResultVisitor
 
 tests_object = {}
@@ -5,17 +6,17 @@ test_list = []
 
 
 class TestStatus(ResultVisitor):
-
     def start_test(self, test):
         global test_list
         object = {
-            'parent': test.parent,
-            'name': test.name,
-            'status': test.status,
-            'message': test.message,
-            'starttime': test.starttime,
-            'endtime': test.endtime,
-            'elapsed(s)': test.elapsedtime/float(1000)
+            "parent": test.parent,
+            "name": test.name,
+            "longname": test.longname,
+            "status": test.status,
+            "message": test.message,
+            "starttime": datetime_to_utc(test.starttime),
+            "endtime": datetime_to_utc(test.endtime),
+            "elapsed(s)": test.elapsedtime / float(1000),
         }
         test_list.append(object)
 
@@ -23,5 +24,5 @@ class TestStatus(ResultVisitor):
     def get_test_status(inpath):
         result = ExecutionResult(inpath)
         result.visit(TestStatus())
-        tests_object['tests'] = test_list
+        tests_object["tests"] = test_list
         return tests_object
